222fd477f258cbb853211dcc1f460f76127e699b
arohaMirai <wslf0115@gmail.com>
2019-10-27 15:05:19 +0800

    auto_backup 2019-10-27 15:05:18

---
+++ automatic_git.bat
@@ -0,0 +1,16 @@
+@echo off
+REM %DATE:~0,10%  2017/07/06
+set dd=%DATE:~0,10%
+set tt=%time:~0,8%
+set hour=%tt:~0,2%
+echo =======================================================
+echo          Starting automatic git commit push
+echo =======================================================
+REM change file directory
+REM start git script 
+echo %~dp0
+git status
+git add .
+git commit -m "auto_backup %dd:/=-% %tt%"
+git pull
+git push origin master
